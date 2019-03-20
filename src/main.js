5bef81134485f56e54f5b14276a1478262b8224f
Matthias Mohr <m.mohr@uni-muenster.de>
2019-03-20 19:26:49 +0100

    Added MigrateCapabilities to main.js, v0.3.4

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 
+const MigrateCapabilities = require('./migrate/capabilities.js');
 const MigrateCollections = require('./migrate/collections.js');
 const MigrateProcesses = require('./migrate/processes.js');
 const FeatureList = require('./featurelist.js');
 const Utils = require('./utils.js');
 
 module.exports = {
+	MigrateCapabilities,
 	MigrateCollections,
 	MigrateProcesses,
 	FeatureList,
